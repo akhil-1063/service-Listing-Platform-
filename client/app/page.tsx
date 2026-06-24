9e0e90352320888a9cffb3f6b4926d3606442129
akhil-1063 <akileshpon@gmail.com>
2026-06-24 14:41:06 +0530

    feat: update root layout, metadata and landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,24 @@
+
 import Navbar from "@/components/sections/Navbar";
+import Footer from "@/components/sections/Footer";
+import HeroSection from "@/components/sections/HeroSection";
+import AboutSection from "@/components/sections/AboutSection";
+import FeaturedServices from "@/components/sections/FeaturedServices";
+
 
 export default function Home() {
   return (
    <div>
-    <Navbar />
+  <Navbar/>
+
+
+<HeroSection/>
+<AboutSection/>
+<FeaturedServices/>
+
+
+
+  <Footer/>
    </div>
   );
 }
